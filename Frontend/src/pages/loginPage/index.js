0a2847638a09b96773ccea3b5034c72290d8b2fe
Guilherme Rocha <guifgr@hotmail.com>
2022-05-09 19:45:14 -0300

    teste

--- Frontend/src/pages/loginPage/index.js
+++ Frontend/src/pages/loginPage/index.js
@@ -59,7 +59,7 @@ export default function SignIn() {
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
-                        Sign in
+                        Logar
                     </Typography>
                     <Box
                         component="form"
