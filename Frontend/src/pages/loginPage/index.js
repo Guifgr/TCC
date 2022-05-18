32e8706561ff55846977ad6321d76dfd0e5f872c
Rômulo Villar Castilho <54681116+rovica@users.noreply.github.com>
2022-05-17 21:15:31 -0300

    Update index.js

--- Frontend/src/pages/loginPage/index.js
+++ Frontend/src/pages/loginPage/index.js
@@ -59,7 +59,7 @@ export default function SignIn() {
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
-                        Logar
+                        Login
                     </Typography>
                     <Box
                         component="form"
